959adfcf84c109d7fab39fb6e342cbd8aaf85dc0
Adidans <adrians.uvis@gmail.com>
2022-08-04 22:39:41 +0300

    Finish game logic

--- script.js
+++ script.js
@@ -12,5 +12,54 @@ function getComputerChoice(){
             choice = 'scissors';
             break;
     }
+    console.log(choice);
     return choice;
+}
+
+function getPlayerChoice() {
+    let input = prompt("Choose Rock Paper or Scissors");
+    let choice = input.toLowerCase();
+    return choice;
+}
+
+function playRound(playerSelection, computerSelection) {
+    let result;
+    switch(true){
+        case playerSelection === 'rock' && computerSelection === 'paper':
+            result = 'You Lose! Paper beats Rock';
+            break;
+        case playerSelection === 'rock' && computerSelection === 'scissors':
+            result = 'You Win! Rock beats Scissors';
+            break;
+        case playerSelection === 'rock' && computerSelection === 'rock':
+            result = 'Tie! Rock ties with Rock';
+            break;
+        case playerSelection === 'paper' && computerSelection === 'rock':
+            result = 'You Win! Paper beats Rock';
+            break;
+        case playerSelection === 'paper' && computerSelection === 'paper':
+            result = 'Tie! Paper ties with Paper';
+            break;
+        case playerSelection === 'paper' && computerSelection === 'scissors':
+            result = 'You Lose! Scissors beats Paper';
+            break;
+        case playerSelection === 'scissors' && computerSelection === 'rock':
+            result = 'You Lose! Rock beats Scissors';
+            break;
+        case playerSelection === 'scissors' && computerSelection === 'paper':
+            result = 'You Win! Scissors beats Paper';
+            break;
+        case playerSelection === 'scissors' && computerSelection === 'scissors':
+            result = 'Tie! Scissors ties with Scissors';
+            break;
+    }
+
+    return result;
+}
+
+function game() {
+    for (let i = 0; i < 5; i++){
+        console.log(playRound(getPlayerChoice(), getComputerChoice()));
+    }
+    
 }
