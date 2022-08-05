dbafc85c3c6bb5761d2c621dd0ff49a4e419a62a
Adidans <adrians.uvis@gmail.com>
2022-08-05 20:39:29 +0300

    Finish script.js

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+const buttons = document.querySelectorAll('button');
+const results = document.querySelector('.results');
+let playerWins = document.createElement('p');
+let computerWins = document.createElement('p');
+let gameWinner = document.createElement('p');
+const playAgain = document.querySelector('#playAgain');
+let playerWinCount = 0;
+let computerWinCount = 0;
+
+
 function getComputerChoice(){
     let num = Math.floor(Math.random()*3)+1;
     let choice;
@@ -15,38 +25,78 @@ function getComputerChoice(){
     return choice;
 }
 
-function getPlayerChoice() {
-    let input = prompt("Choose Rock Paper or Scissors");
-    let choice = input.toLowerCase();
-    return choice;
-}
+const result = document.createElement('p');
+
+playAgain.addEventListener('click', () => {
+    playerWinCount = 0;
+    computerWinCount = 0;
+    gameWinner.textContent = '';
+    buttons.forEach((button) => {
+        button.disabled = false;
+    })
+})
+
+buttons.forEach((button) => {
+    button.addEventListener('click', function handler(){
+        result.textContent = playRound(button.id, getComputerChoice())
+        playerWins.textContent = `Player: ${playerWinCount}`;
+        computerWins.textContent = `Computer: ${computerWinCount}`;
+        if(playerWinCount === 5){
+            gameWinner.textContent = `Player won the game!`
+            results.appendChild(gameWinner);
+            buttons.forEach((button) => {
+                button.disabled = true;
+            })
+            playAgain.disabled = false;
+        }
+        if(computerWinCount === 5){
+            gameWinner.textContent = `Computer won the game!`
+            results.appendChild(gameWinner);
+            buttons.forEach((button) => {
+                button.disabled = true;
+            })
+            playAgain.disabled = false;
+        }
+        results.appendChild(result);
+        results.appendChild(playerWins);
+        results.appendChild(computerWins);
+    });
+});
+
+
 
 function playRound(playerSelection, computerSelection) {
     let result;
     switch(true){
         case playerSelection === 'rock' && computerSelection === 'paper':
             result = 'You Lose! Paper beats Rock';
+            computerWinCount++;
             break;
         case playerSelection === 'rock' && computerSelection === 'scissors':
             result = 'You Win! Rock beats Scissors';
+            playerWinCount++;
             break;
         case playerSelection === 'rock' && computerSelection === 'rock':
             result = 'Tie! Rock ties with Rock';
             break;
         case playerSelection === 'paper' && computerSelection === 'rock':
             result = 'You Win! Paper beats Rock';
+            playerWinCount++;
             break;
         case playerSelection === 'paper' && computerSelection === 'paper':
             result = 'Tie! Paper ties with Paper';
             break;
         case playerSelection === 'paper' && computerSelection === 'scissors':
             result = 'You Lose! Scissors beats Paper';
+            computerWinCount++;
             break;
         case playerSelection === 'scissors' && computerSelection === 'rock':
             result = 'You Lose! Rock beats Scissors';
+            computerWinCount++;
             break;
         case playerSelection === 'scissors' && computerSelection === 'paper':
             result = 'You Win! Scissors beats Paper';
+            playerWinCount++;
             break;
         case playerSelection === 'scissors' && computerSelection === 'scissors':
             result = 'Tie! Scissors ties with Scissors';
@@ -54,11 +104,4 @@ function playRound(playerSelection, computerSelection) {
     }
 
     return result;
-}
-
-function game() {
-    for (let i = 0; i < 5; i++){
-        console.log(playRound(getPlayerChoice(), getComputerChoice()));
-    }
-    
 }
